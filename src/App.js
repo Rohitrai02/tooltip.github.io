284a837ae985400aae06d7438832e7837bf44a98
Rohit Kumar <121882145+Rohitrai02@users.noreply.github.com>
2023-06-19 11:43:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import Main from "./Main";
+import { useState } from "react";
 
 function App() {
   // note:- you can pass bottom, top, left and right as a props for different css styling
+
+  
+  const [type,setType] = useState("top")
+
+  const handleTop = (e)=>{
+    e.preventDefault();
+    alert("Top Button Selected");
+    setType("top")
+  }
+  const handleBottom = (e)=>{
+    e.preventDefault();
+    alert("Bottom Button Selected");
+    setType("bottom")
+  }
+  const handleLeft = (e)=>{
+    e.preventDefault();
+    alert("Left Button Selected");
+    setType("left")
+  }
+
+  const handleRight = (e)=>{
+    e.preventDefault();
+    alert("Right Button Selected");
+    setType("right")
+  }
   return (
     <>
-      <Main type={"bottom"} />
+    <div className="btn">
+      <span>Select Button to Check Hover:</span>
+    <button onClick={handleTop}>Top</button>
+    <button onClick={handleBottom}>Bottom</button>
+    <button onClick={handleLeft}>Left</button>
+    <button onClick={handleRight}>Right</button>
+    </div>
+      <Main type={type} />
     </>
   );
 }
